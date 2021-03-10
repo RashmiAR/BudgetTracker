84db2c55415d9b92f706dc43631aa10fd8d5ab98
Brody <45079557+brodyu@users.noreply.github.com>
2021-03-09 14:55:32 -1000

    Finished landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container rows={3}>
 
-          <Grid.Column width={8}>
-            <Image size='huge' circular src="/images/BudgetTrackerLogo.png"/>
-          </Grid.Column>
+          <Grid.Row>
+            <Image size='massive' circular src="/images/BudgetTrackerLogo.png"/>
+          </Grid.Row>
 
-          <Grid.Column width={8}>
-            <h1>Best Budget Tracker</h1>
-            <p>Sign in or sign up</p>
-          </Grid.Column>
+          <Grid.Row>
+            <Header as='h1'>Best Budget Tracker 1.0</Header>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Header as='h2'>Manage your finances today by signing up!</Header>
+          </Grid.Row>
 
         </Grid>
     );
